c3203270ec45457624e5c5685f2ebc48c0350471
sdabrowski <sdabrow2@gmail.com>
2017-04-19 14:06:36 -0500

    added alternator

---
+++ CodingTemple.April.CarModel/Alternator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTemple.April.CarModel
+{
+    class Alternator : IAlternator
+    {
+        public Alternator(IBattery battery)
+        {
+            Battery = battery;
+        }
+
+        public IBattery Battery { get; set; }
+        
+    }
+
+        //The Alternator charges the battery
+        //Checks if the engine is on in order to charge the battery
+        //bool IsRunning { get; set; } from IEngine should be set to True
+}
